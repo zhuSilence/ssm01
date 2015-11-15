09caa6f51124f4c75b3d70f6fe39f9b4b65b555a
zhuxiang <1347023180@qq.com>
2015-11-15 18:11:46 +0800

    测试git提交

---
+++ src/main/java/com/silence/po/Person.java
@@ -0,0 +1,18 @@
+package com.silence.po;
+
+/**
+ * Created by zhuxiang on 2015/11/15.
+ * Desc :
+ */
+public class Person {
+
+    private String username;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+}
