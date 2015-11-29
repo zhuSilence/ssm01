5b981e59916ed42e76366771c0f5913ede6997f4
Tao <dava060@126.com>
2015-11-29 22:05:56 +0800

    增加设备管理模块相关文件

--- src/main/java/com/silence/mapper/DeviceMapper.java
+++ src/main/java/com/silence/mapper/DeviceMapper.java
@@ -1,6 +1,9 @@
 package com.silence.mapper;
 
 import com.silence.po.Device;
+import com.silence.vo.DeviceQueryVo;
+
+import java.util.List;
 
 public interface DeviceMapper {
     int deleteByPrimaryKey(Integer id) throws Exception;
@@ -14,4 +17,8 @@ public interface DeviceMapper {
     int updateByPrimaryKeySelective(Device record) throws Exception;
 
     int updateByPrimaryKey(Device record) throws Exception;
+
+    List<Device> selectDeviceList(DeviceQueryVo deviceQueryVo) throws Exception;
+
+    int selectAllUserListSize(DeviceQueryVo deviceQueryVo) throws Exception;
 }
